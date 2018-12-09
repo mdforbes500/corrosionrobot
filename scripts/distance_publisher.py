fbd5b49de42c3d81c7925143c2b6f89d9ad5afca
mdforbes500 <mforbes4@gmu.edu>
2018-12-09 18:16:21 -0500

    Starting the distance_publisher

---
+++ scripts/distance_publisher.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+#----------------------------------------#
+# @title: distance_publisher.py
+#
+# @author: Malcolm D. Forbes
+# @email: gmucorrosion@gmail.com
+# @version: v.0.1.0
+#
+# @licsense: MIT
+# @description: Publishes the distance of the ultrasonic sensor
+#----------------------------------------#
