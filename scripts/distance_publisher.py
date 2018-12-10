75238af67dc0569b254f2f98de331832b4f0cafb
mdforbes500 <mforbes4@gmu.edu>
2018-12-09 22:35:15 -0500

    Now including publishing distance measurements.

--- scripts/distance_publisher.py
+++ scripts/distance_publisher.py
@@ -13,8 +13,7 @@
 
 import rospy as rp
 import RPi.GPIO as GPIO
-import time
-from sensor_msgs.msg import uint8
+from std_msgs.msg import uint16
 
 GPIO.setmode(GPIO.BCM)
 GPIO_TRIGGER = 18
@@ -24,4 +23,33 @@ GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
 GPIO.setup(GPIO_ECHO, GPIO.IN)
 
 def distance_publisher():
-    GPIO.output(GPIO_TRIGGER, True)    
+    pub = rp.Publisher('distance', uint16, queue_size=10)
+    rospy.init_node('ultrasonic', anonymous=False)
+    rate = rospy.Rate(10)   #10 Hz
+
+    while not rospy.is_shutdown():
+        GPIO.output(GPIO_TRIGGER, True)
+        time.sleep(0.00001)
+        GPIO.output(GPIO_TRIGGER, False)
+
+        start_time = rospy.get_time()
+        stop_time = rospy.get_time()
+
+        while GPIO.input(GPIO_ECHO) == 0:
+            start_time = rospy.get_time()
+
+        while GPIO.input(GPIO_ECHO) == 1:
+            stop_time = rospy.get_time()
+
+        time_elapsed = stop_time - start_time
+        distance = (time_elapsed * 34300)/2
+        distance_str = "%s" % distance
+        rospy.loginfo(distance_str)
+        pub.publish(distance_str)
+        rate.sleep()
+
+if __name__ = '__main__':
+    try:
+        distance_publisher()
+    except rospy.ROSInterruptException:
+        pass
