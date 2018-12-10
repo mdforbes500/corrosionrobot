ef3ba9699323a8c3afa089436f1ac599d5ae6ec2
mdforbes500 <mforbes4@gmu.edu>
2018-12-09 21:03:28 -0500

    hehe. i did something stupid with callback

--- scripts/system_subscriber.py
+++ scripts/system_subscriber.py
@@ -15,22 +15,15 @@ from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 import cv2 as cv
 
-def callbackLeft(data):
-    br_left = CvBridge()
-    rp.loginfo('recieving image left')
-    cv.imshow('camera_driver', br_left.imgmsg_to_cv2(data))
-    cv.waitKey(5)
-
-def callbackRight(data):
-    br_right = CvBridge()
-    rp.loginfo('recieveing image right')
-    cv.imshow('camera_driver', br_right.imgmsg_to_cv2(data))
+def callback(data):
+    br = CvBridge()
+    rp.loginfo('recieving image')
+    cv.imshow('camera_driver', br.imgmsg_to_cv2(data))
     cv.waitKey(5)
 
 def cv_listener():
     rp.init_node('listener', anonymous=False)
-    rp.Subscriber('image_raw_left', Image, callbackLeft)
-    rp.Subscriber('image_raw_right', Image, callbackRight)
+    rp.Subscriber('image_raw', Image, callback)
     rp.spin()
     cv.destroyAllWindows()
 
