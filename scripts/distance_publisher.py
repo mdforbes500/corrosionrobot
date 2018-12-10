67aaf117a145ce872bd7eee714c4ea4eba9dc29a
mdforbes500 <mforbes4@gmu.edu>
2018-12-09 19:58:30 -0500

    edited CMakeLists

--- scripts/distance_publisher.py
+++ scripts/distance_publisher.py
@@ -10,3 +10,18 @@
 # @licsense: MIT
 # @description: Publishes the distance of the ultrasonic sensor
 #----------------------------------------#
+
+import rospy as rp
+import RPi.GPIO as GPIO
+import time
+from sensor_msgs.msg import uint8
+
+GPIO.setmode(GPIO.BCM)
+GPIO_TRIGGER = 18
+GPIO=ECHO = 24
+
+GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
+GPIO.setup(GPIO_ECHO, GPIO.IN)
+
+def distance_publisher():
+    GPIO.output(GPIO_TRIGGER, True)    
