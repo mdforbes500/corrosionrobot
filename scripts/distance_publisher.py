8420cd85572aad9fceb663cec1ed65976c1da653
mdforbes500 <mforbes4@gmu.edu>
2018-12-10 22:26:00 -0500

    modified:   scripts/distance_publisher.py

--- scripts/distance_publisher.py
+++ scripts/distance_publisher.py
@@ -13,43 +13,43 @@
 
 import rospy as rp
 import RPi.GPIO as GPIO
-from std_msgs.msg import uint16
+from std_msgs.msg import String
 
 GPIO.setmode(GPIO.BCM)
 GPIO_TRIGGER = 18
-GPIO=ECHO = 24
+GPIO_ECHO = 24
 
 GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
 GPIO.setup(GPIO_ECHO, GPIO.IN)
 
 def distance_publisher():
-    pub = rp.Publisher('distance', uint16, queue_size=10)
-    rospy.init_node('ultrasonic', anonymous=False)
-    rate = rospy.Rate(10)   #10 Hz
+    pub = rp.Publisher('distance', String, queue_size=10)
+    rp.init_node('ultrasonic', anonymous=False)
+    rate = rp.Rate(10)   #10 Hz
 
-    while not rospy.is_shutdown():
+    while not rp.is_shutdown():
         GPIO.output(GPIO_TRIGGER, True)
-        time.sleep(0.00001)
+        rate.sleep()
         GPIO.output(GPIO_TRIGGER, False)
 
-        start_time = rospy.get_time()
-        stop_time = rospy.get_time()
+        start_time = rp.get_time()
+        stop_time = rp.get_time()
 
         while GPIO.input(GPIO_ECHO) == 0:
-            start_time = rospy.get_time()
+            start_time = rp.get_time()
 
         while GPIO.input(GPIO_ECHO) == 1:
-            stop_time = rospy.get_time()
+            stop_time = rp.get_time()
 
         time_elapsed = stop_time - start_time
         distance = (time_elapsed * 34300)/2
         distance_str = "%s" % distance
-        rospy.loginfo(distance_str)
+        rp.loginfo(distance_str)
         pub.publish(distance_str)
         rate.sleep()
 
 if __name__ == '__main__':
     try:
         distance_publisher()
-    except rospy.ROSInterruptException:
+    except rp.ROSInterruptException:
         pass
