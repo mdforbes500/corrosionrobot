eda1dbfe8a359e9bc040c0bdb877ff250bb8b9aa
mdforbes500 <mforbes4@gmu.edu>
2018-12-10 21:55:52 -0500

    Added second callback for the distance.

--- scripts/system_subscriber.py
+++ scripts/system_subscriber.py
@@ -15,15 +15,19 @@ from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 import cv2 as cv
 
-def callback(data):
+def callback1(data):
     br = CvBridge()
     rp.loginfo('recieving image')
     cv.imshow('camera_driver', br.imgmsg_to_cv2(data))
     cv.waitKey(5)
 
+def callback2(data):
+    rp.loginfo('recieving distance %s', data.data)
+
 def cv_listener():
     rp.init_node('listener', anonymous=False)
-    rp.Subscriber('image_raw', Image, callback)
+    rp.Subscriber('image_raw', Image, callback1)
+    rp.Subscriber('distance', uint16, callback2)
     rp.spin()
     cv.destroyAllWindows()
 
