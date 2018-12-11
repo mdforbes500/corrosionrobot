12280a536905f9ff94205cb9b007b2fc5acee39a
mdforbes500 <mforbes4@gmu.edu>
2018-12-10 22:32:47 -0500

    for realz this time

--- scripts/system_subscriber.py
+++ scripts/system_subscriber.py
@@ -11,7 +11,8 @@
 #----------------------------------------#
 
 import rospy as rp
-from sensor_msgs.msg import Image, String
+from sensor_msgs.msg import Image
+from std_msgs.msg import String
 from cv_bridge import CvBridge
 import cv2 as cv
 
