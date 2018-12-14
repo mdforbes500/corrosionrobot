1180340be722b20e750ac7524a70fe935aebdc6e
Malcolm Forbes <mforbes4@gmu.edu>
2018-12-14 00:04:48 -0500

    Not setup for python3

--- scripts/image_proc2.py
+++ scripts/image_proc2.py
@@ -28,7 +28,7 @@ def image_proc2(fh=args[1], size=args[2]):
 
 	pixel_size = (image_width/float(pixel_number_wide))**2
 
-	print "Pixel size: {0} ".format(pixel_size)
+	print("Pixel size: {0} ".format(pixel_size))
 	pixels = list(pic.getdata())
 	counter = 0
 	corCount = 0
@@ -39,7 +39,7 @@ def image_proc2(fh=args[1], size=args[2]):
 		if(x[0]+x[1]+x[2] < 300):
 			corCount+=1
 
-	print "Number of pixels: {0}\n".format(counter)
-	print "Number of corrosion pixels: {0}\n".format(corCount)
+	print("Number of pixels: {0}\n".format(counter))
+	print("Number of corrosion pixels: {0}\n".format(corCount))
 	corrosion_area = pixel_size*corCount
-	print "Total Area of Corrosion: {0} Square Centimeters".format(corrosion_area)
+	print("Total Area of Corrosion: {0} Square Centimeters".format(corrosion_area))
