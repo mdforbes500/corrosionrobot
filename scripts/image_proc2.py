383fbd7ba82288c2d6ae7588cd6eca2198c480a0
Malcolm Forbes <mforbes4@gmu.edu>
2018-12-14 01:54:03 -0500

    I fixed this thing... now it works with given distance data.

--- scripts/image_proc2.py
+++ scripts/image_proc2.py
@@ -12,34 +12,37 @@
 # the corrosion.
 #----------------------------------------#
 
-from sensor_msgs.msg import Image as img
 from PIL import Image
 import sys
 from PIL import ImageFilter
 
-def image_proc2(args):
-	image = args[1]
-	pic = Image.open(image, 'r')
-	pixel_number_wide = pic.size[0]
-	image_width = (1.50+1.70)/3.04*(3280/3.68)*(680/3280)*float(args[2])
+def main(args):
+    image = args[1]
+    pic = Image.open(image, 'r')
+    width = float(pic.size[0]) #px
+    sensor_size = 0.386 #cm
+    focal_length = 0.304 #cm
+    distance = float(args[2]) #cm
+    image_width = sensor_size*distance/focal_length*640/2464 #cm
+    print("Reading '{0}'...".format(args[1]))
+    print("Distance from object: {0} cm".format(args[2]))
+    print("Image width: {0:.{1}f} cm".format(image_width,3))
 
-	pixel_size = (image_width/float(pixel_number_wide))**2
+    pixel_area = (image_width/width)**2
+    pixels = list(pic.getdata())
+    counter = 0
+    corCount = 0
 
-	print("Pixel size: {0} ".format(pixel_size))
-	pixels = list(pic.getdata())
-	counter = 0
-	corCount = 0
+    for x in pixels:
+        if(x[0]+x[1]+x[2] >= 0):
+            counter+=1
+        if(x[0]+x[1]+x[2] < 300):
+            corCount+=1
 
-	for x in pixels:
-		if(x[0]+x[1]+x[2] >= 0):
-			counter+=1
-		if(x[0]+x[1]+x[2] < 300):
-			corCount+=1
+    print("Number of pixels: {0:d} pixels".format(counter))
+    print("Number of corrosion pixels: {0:d} pixels".format(corCount))
+    corrosion_area = pixel_area*corCount
+    print("Total Area of Corrosion: {0:.{1}f} cm^2".format(corrosion_area,3))
 
-	print("Number of pixels: {0}\n".format(counter))
-	print("Number of corrosion pixels: {0}\n".format(corCount))
-	corrosion_area = pixel_size*corCount
-	print("Total Area of Corrosion: {0} Square Centimeters".format(corrosion_area))
 
-if __name__ == "main":
-    image_proc2(sys.argv)
+main(sys.argv)
