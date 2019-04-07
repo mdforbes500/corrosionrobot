93fb2e439082065eb89eedf1c4b3b8322b7642ea
JHawk66 <44930857+JHawk66@users.noreply.github.com>
2019-04-07 13:31:13 -0400

    Temp changing image_publisher to try to operatate with threading to get better FPS and latency

--- scripts/image_publisher.py
+++ scripts/image_publisher.py
@@ -11,6 +11,10 @@
 #----------------------------------------#
 
 import rospy as rp
+from imutils.video import FileVideoStream
+from imutils.video import FPS
+import imutils
+import time
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 import cv2 as cv
@@ -19,22 +23,62 @@ import sys
 myargs = rp.myargv(argv=sys.argv)
 
 def cv_publisher(index=myargs[1]):
+    fvs = FileVideoStream(int(index)).start()
+    time.sleep(1)
     pub = rp.Publisher('image_raw', Image, queue_size=10)
     rp.init_node('camera_driver', anonymous=False)
     rate = rp.Rate(1) #60 Hz
-    cap = cv.VideoCapture(int(index))
     br = CvBridge()
-
     while not rp.is_shutdown():
-        [ret, frame] = cap.read()
-        if ret == True:
+        while fvs.more():
+            frame = fvs.read()
             rp.loginfo('publish image')
             pub.publish(br.cv2_to_imgmsg(frame))
-
-        rate.sleep()
+            rate.sleep()
 
 if __name__ == '__main__':
     try:
         cv_publisher()
     except rp.ROSInterruptException:
         pass
+
+
+
+
+
+
+
+
+
+
+
+######################OLD CODE FOR IMAGE_PUBLISHER.PY##############################
+
+# import rospy as rp
+# from sensor_msgs.msg import Image
+# from cv_bridge import CvBridge
+# import cv2 as cv
+# import sys
+#
+# myargs = rp.myargv(argv=sys.argv)
+#
+# def cv_publisher(index=myargs[1]):
+#     pub = rp.Publisher('image_raw', Image, queue_size=10)
+#     rp.init_node('camera_driver', anonymous=False)
+#     rate = rp.Rate(1) #60 Hz
+#     cap = cv.VideoCapture(int(index))
+#     br = CvBridge()
+#
+#     while not rp.is_shutdown():
+#         [ret, frame] = cap.read()
+#         if ret == True:
+#             rp.loginfo('publish image')
+#             pub.publish(br.cv2_to_imgmsg(frame))
+#
+#         rate.sleep()
+#
+# if __name__ == '__main__':
+#     try:
+#         cv_publisher()
+#     except rp.ROSInterruptException:
+#         pass
