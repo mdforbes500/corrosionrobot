c62aeecdf32e47524b88ab0453b706242bef5485
JHawk66 <44930857+JHawk66@users.noreply.github.com>
2019-04-08 21:18:38 -0400

    fixed something

--- scripts/motorTest.py
+++ scripts/motorTest.py
@@ -263,9 +263,9 @@ def allStop():
 	rotate_kit.motor2.throttle = 0
 	rotate_kit.motor3.throttle = 0
 	rotate_kit.motor4.throttle = 0
-	servo_kit.continuous_servo[rack_servo].throttle = 0
-	servo_kit.continuous_servo[front_package].throttle = 0
-	servo_kit.continuous_servo[rear_package].throttle = 0
+	servo_kit_c.continuous_servo[rack_servo].throttle = 0
+	servo_kit_c.continuous_servo[front_package].throttle = 0
+	servo_kit_c.continuous_servo[rear_package].throttle = 0
 
 
 
