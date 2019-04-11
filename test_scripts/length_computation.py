e513cbfa0874f7dd805e37ff7d0616499a44b11c
mdforbes500 <malcolm.forbes500@gmail.com>
2019-04-10 22:31:28 -0400

    Now it detects each site.

--- test_scripts/length_computation.py
+++ test_scripts/length_computation.py
@@ -18,7 +18,148 @@ from skimage import measure
 import cv2 as cv
 import numpy as np
 import math
+import imutils
 import sys
+
+def compute_length(image, w, h, d):
+    # Field of View for PiCamera V2
+    FOV = (62.2, 48.8) #deg: horizontal X vertical
+
+    width = np.size(image, 1) #px
+    height = np.size(image, 0) #px
+
+    #Angle of one pixel for both height and width
+    w_angle = 1/width*FOV[0] #deg
+    h_angle = 1/height*FOV[1] #deg
+
+    #Width and Height of one pixel in cm
+    px_width = d*math.tan(math.radians(w_angle)) #cm
+    px_height = d*math.tan(math.radians(h_angle)) #cm
+
+    #Computation of width and height of anamoly in inches
+    real_width = px_width*w/2.54
+    real_height = px_height*h/2.54
+
+    return real_width, real_height
+
+def connected_component(image, raw_image):
+    #corrosion_thresh = cv.bitwise_not(corrosion_thresh) #reverse colors
+
+    # perform a connected component analysis on the thresholded
+    # image, then initialize a mask to store only the "large"
+    # components
+    labels = measure.label(image, neighbors=8, background=0)
+    mask = np.zeros(image.shape, dtype="uint8")
+
+    # loop over the unique components
+    for label in np.unique(labels):
+        #if this is the background label, ignore it
+        if label == 0:
+            continue
+        # otherwise, construct the label mask and count the
+        # number of pixels
+        label_mask = np.zeros(image.shape, dtype="uint8")
+        label_mask[labels == label] = 255
+        num_pixels = cv.countNonZero(label_mask)
+
+        # if the number of pixels in the component is sufficiently
+        # large, then add it to our mask of "large blobs"
+        if num_pixels > 50:
+            mask = cv.add(mask, label_mask)
+
+    if (cv.__version__ == "3.3.0"):
+        contours= cv.findContours(mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+    else:
+        contours= cv.findContours(mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+
+    cnts = imutils.grab_contours(contours)
+    #cnts = contours.sort_contours(cnts)[0]
+    # loop over the contours
+    characteristics = []
+    for (i, cnt) in enumerate(cnts):
+        # plot bounding boxes
+        (x, y, w, h) = cv.boundingRect(cnt)
+        rect = cv.minAreaRect(cnt)
+        box = cv.boxPoints(rect)
+        box = np.int0(box)
+        im = cv.drawContours(raw_image,[box],0,(0,0,255),2)
+        # add width and height to characteristics
+        characteristics.append([w,h])
+        # compute the center of the contour area and draw a circle
+        # representing the center
+        M = cv.moments(cnt)
+        cX = int(M["m10"] / M["m00"])
+        cY = int(M["m01"] / M["m00"])
+
+        # draw the countour number on the image
+        cv.putText(raw_image, "#{}".format(i), (cX - 20, cY), cv.FONT_HERSHEY_SIMPLEX,
+		1.0, (0, 255, 0), 2)
+
+    cv.imshow('Corrosion Contours', im)
+    cv.waitKey(0)
+    cv.destroyWindow('Corrosion Contours')
+
+    return characteristics
+
+def gray_filter(image, thresh=127, inverse=False):
+    gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+    blurred = cv.GaussianBlur(gray, (11, 11), 0)
+    # threshold the image to reveal light regions in the
+    # blurred image
+    if inverse == True:
+        thresh = cv.threshold(blurred, thresh, 255, cv.THRESH_BINARY_INV)[1]
+    else:
+        thresh = cv.threshold(blurred, thresh, 255, cv.THRESH_BINARY)[1]
+    # perform a series of erosions and dilations to remove
+    # any small blobs of noise from the thresholded image
+    thresh = cv.erode(thresh, None, iterations=2)
+    thresh = cv.dilate(thresh, None, iterations=4)
+
+    return thresh
+
+def YCrCb_filter(image, channel='cb', thresh=127):
+    #Conversion from Blue Green Red to
+    #Luminance, Red Correction, Blue Correction
+    image_ycb = cv.cvtColor(image, cv.COLOR_BGR2YCrCb)
+    cv.imshow("YCrCb Colorspace", image_ycb)
+    cv.waitKey(0)
+    cv.destroyWindow("YCrCb Colorspace")
+
+    #Split colorspace into three channels
+    y,cr,cb = cv.split(image_ycb)
+
+    #Apply Gaussian Blur to each channel
+    if channel == 'Y':
+    #Y: Luminance channel
+        y_blur = cv.GaussianBlur(y, (11, 11), 0)
+        output = y_blur
+        cv.imshow("Luminance Blurred", y_blur)
+        cv.waitKey(0)
+        cv.destroyWindow("Luminance Blurred")
+    elif channel == 'Cb':
+    #Cb: Blue Corrected
+        cb_blur = cv.GaussianBlur(cb, (11, 11), 0)
+        output = cb_blur
+        cv.imshow("Red Correction Blurred", cb_blur)
+        cv.waitKey(0)
+        cv.destroyWindow("Red Correction Blurred")
+    elif channel == 'Cr':
+    #Cr: Red Corrected
+        cr_blur = cv.GaussianBlur(cr, (11, 11), 0)
+        output = cr_blur
+        cv.imshow("Blue Correction Blurred", cr_blur)
+        cv.waitKey(0)
+        cv.destroyWindow("Blue Correction Blurred")
+    else:
+        print("Please select valid channel")
+        return None
+
+    #Threshold to remove light sections
+    ret,threshold = cv.threshold(output,thresh,255,0)
+    threshold = cv.erode(threshold, None, iterations=2)
+    threshold = cv.dilate(threshold, None, iterations=4)
+
+    return threshold
 
 def main(args):
     #Inputs from system arguments
@@ -27,170 +168,23 @@ def main(args):
     distance = float(args[2]) #cm
     print("Distance from object: {0} cm".format(distance))
 
-    #Opening filehandle for reading and saving in memory as grayscale image
-    # then applying gaussian blur to smooth image boundaries
+    #Opening filehandle for reading and saving in memory
     image = cv.imread(filehandle)
-    cv.imshow("image", image)
+    cv.imshow("Raw Image", image)
     cv.waitKey(0)
-    cv.destroyWindow("image")
-    image_ycb = cv.cvtColor(image, cv.COLOR_BGR2YCrCb)
-    cv.imshow("image_ycb", image_ycb)
-    cv.waitKey(0)
-    cv.destroyWindow("image_ycb")
-    y,c,b = cv.split(image_ycb)
+    cv.destroyWindow("Raw Image")
 
-    # define range of red color in HSV
-    #lower_red = np.array([46,25,15])
-    #upper_red = np.array([53,80,60])
+    filtered = gray_filter(image, thresh=80, inverse=True)
+    #cb_blur = YCrCb_filter(image, channel='Cb', thresh=127)
+    characteristics = connected_component(filtered, image)
 
-    # Threshold the HSV image to get only red colors
-    #mask = cv.inRange(image_ycb, lower_red, upper_red)
-
-    # Bitwise-AND mask and original image
-    #blurred = cv.GaussianBlur(mask, (11, 11), 0)
-    cv.imshow("Coating Damage", b)
-    cv.waitKey(0)
-    cv.destroyWindow("Coating Damage")
-    cv.imshow("Gaussian Filtering", c)
-    cv.waitKey(0)
-    cv.destroyWindow("Gaussian Filtering")
-
-    #===============================CORROSION=================================#
-    #Softening threshold values to again lower noisy edges
-    ret,corrosion_thresh = cv.threshold(blurred,127,255,0)
-    cv.imshow("Gray Threshold", corrosion_thresh)
-    cv.waitKey(0)
-    cv.destroyWindow("Gaussian Filtering")
-    corrosion_thresh = cv.erode(corrosion_thresh, None, iterations=2)
-    cv.imshow("Gray Threshold", corrosion_thresh)
-    cv.waitKey(0)
-    cv.destroyWindow("Gaussian Filtering")
-    corrosion_thresh = cv.dilate(corrosion_thresh, None, iterations=4)
-    cv.imshow("Gray Threshold", corrosion_thresh)
-    cv.waitKey(0)
-    cv.destroyWindow("Gaussian Filtering")
-    corrosion_thresh = cv.bitwise_not(corrosion_thresh) #reverse colors
-    cv.imshow("Gray Threshold", corrosion_thresh)
-    cv.waitKey(0)
-    cv.destroyWindow("Gaussian Filtering")
-
-    # perform a connected component analysis on the thresholded
-    # image, then initialize a mask to store only the "large"
-    # components
-    corrosion_labels = measure.label(corrosion_thresh, neighbors=8, background=0)
-    corrosion_mask = np.zeros(corrosion_thresh.shape, dtype="uint8")
-
-    # loop over the unique components
-    for corrosion_label in np.unique(corrosion_labels):
-        #if this is the background label, ignore it
-        if corrosion_label == 0:
-            continue
-
-	# otherwise, construct the label mask and count the
-	# number of pixels
-    corrosion_label_mask = np.zeros(corrosion_thresh.shape, dtype="uint8")
-    corrosion_label_mask[corrosion_labels == corrosion_label] = 255
-    num_pixels = cv.countNonZero(corrosion_label_mask)
-
-    # if the number of pixels in the component is sufficiently
-    # large, then add it to our mask of "large blobs"
-    if num_pixels > 300:
-        corrosion_mask = cv.add(corrosion_mask, corrosion_label_mask)
-
-    cv.imshow('Corrosion Threshold image', corrosion_mask)
-    cv.waitKey(0)
-    cv.destroyWindow('Corrosion Threshold image')
-    if (cv.__version__ == "3.3.0"):
-        (_,corrosion_contours,heirarchy_c)= cv.findContours(corrosion_mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
-    else:
-        (corrosion_contours,heirarchy_c)= cv.findContours(corrosion_mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
-    cnt_c = corrosion_contours[0]
-    x_c,y_c,w_c,h_c = cv.boundingRect(cnt_c)
-    print("Corrosion width is: {0}\nCorrosion height is: {1}".format(w_c, h_c))
-
-    #Plot bounding box
-    rect_c = cv.minAreaRect(cnt_c)
-    box_c = cv.boxPoints(rect_c)
-    box_c = np.int0(box_c)
-    im_corrosion = cv.drawContours(image,[box_c],0,(0,0,255),2)
-    cv.imshow('Corrosion Contours', im_corrosion)
-    cv.waitKey(0)
-    cv.destroyWindow('Corrosion Contours')
-
-    #===============================COATING===================================#
-    ret2,coating_thresh = cv.threshold(blurred,127,255,0)
-    coating_thresh = cv.erode(coating_thresh, None, iterations=2)
-    coating_thresh = cv.dilate(coating_thresh, None, iterations=2)
-    coating_thresh = cv.bitwise_not(coating_thresh) #reverse colors
-    #coating_thresh = cv.bitwise_not(coating_thresh)
-
-    # perform a connected component analysis on the thresholded
-    # image, then initialize a mask to store only the "large"
-    # components
-    coating_labels = measure.label(coating_thresh, neighbors=8, background=0)
-    coating_mask = np.zeros(coating_thresh.shape, dtype="uint8")
-
-    # loop over the unique components
-    for coating_label in np.unique(coating_labels):
-        #if this is the background label, ignore it
-        if coating_label == 0:
-            continue
-
-	# otherwise, construct the label mask and count the
-	# number of pixels
-    coating_label_mask = np.zeros(coating_thresh.shape, dtype="uint8")
-    coating_label_mask[coating_labels == coating_label] = 255
-    num_pixels = cv.countNonZero(coating_label_mask)
-
-    # if the number of pixels in the component is sufficiently
-    # large, then add it to our mask of "large blobs"
-    if num_pixels > 300:
-        coating_mask = cv.add(coating_mask, coating_label_mask)
-
-    cv.imshow('Coating Threshold image', coating_mask)
-    cv.waitKey(0)
-    cv.destroyWindow('Coating Threshold image')
-
-    if (cv.__version__ == "3.3.0"):
-        (_,coating_contours, heirarchy_ct) = cv.findContours(coating_mask, cv.RETR_CCOMP, cv.CHAIN_APPROX_SIMPLE)
-    else:
-        (coating_contours, heirarchy_ct) = cv.findContours(coating_mask, cv.RETR_CCOMP, cv.CHAIN_APPROX_SIMPLE)
-    cnt_ct = coating_contours[0]
-    x_ct,y_ct,w_ct,h_ct = cv.boundingRect(cnt_ct)
-    print("Coating width is: {0}\nCoating height is: {1}".format(w_ct, h_ct))
-
-    #Plot bounding box
-    rect_ct = cv.minAreaRect(cnt_ct)
-    box_ct = cv.boxPoints(rect_ct)
-    box_ct = np.int0(box_ct)
-    im_coating = cv.drawContours(image,[box_ct],0,(0,0,255),2)
-    cv.imshow('Coating Contours', im_coating)
-    cv.waitKey(0)
-    cv.destroyWindow('Coating Contours')
-    #===========================Length Computation============================#
-    width = np.size(image_gray, 1) #px
-    height = np.size(image_gray, 0) #px
-
-    # Field of View for PiCamera V2
-    FOV = (62.2, 48.8) #deg: horizontal X vertical
-
-    #Angle of one pixel for both height and width
-    w_angle = 1/width*FOV[0] #deg
-    h_angle = 1/height*FOV[1] #deg
-
-    #Width and Height of one pixel in cm
-    px_width = distance*math.tan(math.radians(w_angle)) #cm
-    px_height = distance*math.tan(math.radians(h_angle)) #cm
-
-    #Computation of width and height of corrosion anamoly
-    corrosion_width = px_width*w_c/2.54
-    corrosion_height = px_height*h_c/2.54
-    print("Corrosion width is: {0.3}\nCorrosion height is: {1.3}".format(corrosion_width, corrosion_height))
-
-    #Computation of width and height of coating damage
-    coating_width = px_width*w_ct/2.54
-    coating_height = px_height*h_ct/2.54
-    print("Coating width is: {0.3}\nCoating height is: {1.3}".format(coating_width, coating_height))
+    anomalies = []
+    for index, anomaly in enumerate(characteristics):
+        width_corrosion, height_corrosion = compute_length(image, anomaly[0], anomaly[1], distance)
+        anomalies.append([width_corrosion, height_corrosion])
+        print("Site {0}:".format(index))
+        print("Corrosion width: {0:.3f} in.".format(width_corrosion))
+        print("Corrosion height: {0:.3f}in.".format(height_corrosion))
 
 if __name__ == "__main__":
     main(sys.argv)
